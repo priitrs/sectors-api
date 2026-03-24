3ef4a2c513c3dd5bedbf046fd17c47d3a03e6a8e
Priit Roosmäe <priitrs@gmail.com>
2026-03-24 07:05:00 +0200

    Improve naming

--- src/main/java/com/sectors/api/service/CustomUserDetailsService.java
+++ src/main/java/com/sectors/api/service/CustomUserDetailsService.java
@@ -8,19 +8,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.security.core.userdetails.User.*;
+
 @Service
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository repo;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) {
-        User user = repo.findByUsername(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        return org.springframework.security.core.userdetails.User
-                .withUsername(user.getUsername())
+        return withUsername(user.getUsername())
                 .password(user.getPassword())
                 .roles("USER")
                 .build();
