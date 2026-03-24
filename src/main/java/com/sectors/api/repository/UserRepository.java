e708c0c3fefa8e47eaf9f9bf9587c54f38094559
Priit Roosmäe <priitrs@gmail.com>
2026-03-24 20:16:59 +0200

    Fix JPA IDs

--- src/main/java/com/sectors/api/repository/UserRepository.java
+++ src/main/java/com/sectors/api/repository/UserRepository.java
@@ -4,7 +4,8 @@ import com.sectors.api.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
 }
